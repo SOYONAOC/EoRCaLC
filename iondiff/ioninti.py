cab438d353459ecef2e98800c198ac854c3deac5
SOYONAOC <onmyjiflow@gmail.com>
2025-09-16 21:17:28 +0800

    Add new fuction

--- iondiff/ioninti.py
+++ iondiff/ioninti.py
@@ -12,108 +12,50 @@ from astropy.constants import m_p
 
 class Ion:
 
-    def __init__(self,z,fesc=0.2,kakaka=1.4*1e-28,xi_ion=10**25.6,A2byA1=0.1,ktrans=200,alpha=2.0,beta=0.0):
+    def __init__(self,z,fesc=0.2,qion=4000,A2byA1=0.1,ktrans=200,alpha=2.0,beta=0.0):
         self.cosmo = MassFunctions(A2byA1=A2byA1,kMpc_trans=ktrans,alpha=alpha,beta=beta)
         self.A2byA1 = A2byA1
         self.kMpc_trans = ktrans
         self.alpha = alpha
         self.beta = beta
         self.z = z
-        self.kakaka = kakaka
-        self.xi_ion = xi_ion
+        self.mH = (self.cosmo.mHu.to(u.M_sun)).value #M_sun
+        self.ob0 = self.cosmo.omegab
+        self.om0 = self.cosmo.omegam
+        self.nH = self.cosmo.nH  #cm^-3
         self.fesc = fesc
         self.deltaV_interp = np.linspace(-0.999, 2, 1000)  # delta_V
         self.M_min = self.cosmo.M_vir(0.61, 1e4, z)  # M_sun
+        self.M_max = self.cosmo.M_vir(0.61, 2e8, z)
         self.M_J = self.cosmo.M_Jeans(z)  # M_sun
-        self.qion = 4000
-
+        self.qion = qion
 
     ### Source term
-
-
-
-    ### IGM term
-    def n_HI(self,deltaV):
-        return n_H(deltaV).to(u.Mpc**-3).value  ### coming number density of hydrogen atoms in cm^-3
+    def nion_interp(self,Mv:float,deltaV:np.ndarray)->np.ndarray:
+        def diff(m,Mv,deltaR):
+            return self.fstar(m)*m*self.cosmo.dndmeps(m,Mv,deltaR,self.z)/ self.mH * self.ob0 / self.om0
+        return self._nion_trapez(diff,Mv,deltaV)*self.qion*self.fesc
     
-    def CHII(self,z):
-        return 2.9*((1+z)/6)**-1.1
-
-    def dtdz(self,z):
-        return ((-1/(H(z)*(1+z))).to(u.s)).value
-
-    def dnrec_dz_path(self,deltaV,xHII_Field:np.ndarray)->np.ndarray:
-        x_HE = 1.08
-        # CIGM = self.CHII(z)
-        CIGM = 3.0
-        nh = self.n_HI(deltaV)*(u.Mpc**-3).to(u.cm**-3)
-        Q_HII = xHII_Field
-        alpha_A = 4.2e-13 #cm**3/s
-        # alpha_B = 2.59e-13  # cm^3/s at 10^4 K
-        differential_trans = self.dtdz(self.z)
-        return -CIGM*x_HE*alpha_A*nh*Q_HII*(1+self.z)**3 * differential_trans
+    def nion_st(self,z):
+        def diff(m):
+            return self.fstar(m)*m*self.cosmo.dndmst(m,z)/ self.mH * self.ob0 / self.om0
+        x = np.linspace(np.log10(self.M_min),np.log10(self.M_max),1000)
+        y = diff(10**x)*10**x*np.log(10)
+        return np.trapezoid(y,x)*self.qion*self.fesc
     
     ### Minihalo term
-    def dndmeps(self,M,Mr,deltaV,z):
-        deltaL = self.cosmo.delta_L(deltaV,self.z)
-        sig1 = self.cosmo.sigma2_interp(M) - self.cosmo.sigma2_interp(Mr)
-        del1 = self.cosmo.deltac(z) - deltaL
-        return self.cosmo.rhom * (1 + deltaV) / M / np.sqrt(2 * np.pi) * abs(self.cosmo.dsigma2_dm_interp(M)) * del1 / (sig1**(3 / 2)) * np.exp(-del1 ** 2 / (2 * sig1))
-
-    def dEPS_dz(self,M,Mv,deltaV,z):
-        return (self.dndmeps(M,Mv,deltaV,z+0.001*z) - self.dndmeps(M,Mv,deltaV,z-0.001*z)) / (0.002*z)
-
-    def Xim(self,m,z):
-        A,B,C,D,E,F,G = 4.4 , 0.334, 0.023, 0.199, -0.042, 0,1
-        M7 = m/1e7
-        F0 = 1.0
-        return 1+A*M7**(B+C*np.log10(M7))*(F+G*(1+z)/10)* F0**(D+E*np.log10(F0))
+    def eps_dz(self,M,Mv,deltaV,z):
+        return (self.cosmo.dndmeps(M,Mv,deltaV,z+0.001*z) - self.cosmo.dndmeps(M,Mv,deltaV,z-0.001*z)) / (0.002*z)
     
-    def dNxi_dz(self,m,deltaR,Mv,z):
-        m_H = (m_p.to(u.M_sun)).value #M_sun
-        return self.Xim(m,z)*m*self.dEPS_dz(m,Mv,deltaR,z)/ m_H * self.cosmo.omegab / self.cosmo.omegam
-
-    
-    def Simpson_Nxi_dz(self,delta_R:np.ndarray,M_v:float,z:float) -> np.ndarray:
-        try:
-            Nxi_arr = np.load(f'.Nxi_dz_Interp_init/NxiAtz{self.z:.2f}/Nxi_arr_Mv_{M_v:.3f}at_z={self.z:.2f}_A{self.A2byA1}_k{self.kMpc_trans}_alpha{self.alpha}_beta{self.beta}.npy')
-        except FileNotFoundError:
-            os.makedirs(f'.Nxi_dz_Interp_init/NxiAtz{self.z:.2f}', exist_ok=True)
-            M_min = self.M_J
-            M_max = self.M_min
-            Mh_interp = np.linspace(np.log10(M_min), np.log10(M_max), 1000)  # log10(M)
-            deltaR_grid, m_grid = np.meshgrid(self.deltaV_interp, Mh_interp, indexing='ij')
-            mh = self.dNxi_dz(m=10**m_grid, deltaR=deltaR_grid, Mv=M_v, z=z)
-            mh[mh > 0] = 0
-            integrand = mh * 10**m_grid * np.log(10)  # dN/dlogM -> dN/dM
-            # Integrate using Simpson's rule
-            Nxi_arr_save = simpson(integrand, x=Mh_interp, axis=1)
-            np.save(f'.Nxi_dz_Interp_init/NxiAtz{self.z:.2f}/Nxi_arr_Mv_{M_v:.3f}at_z={self.z:.2f}_A{self.A2byA1}_k{self.kMpc_trans}_alpha{self.alpha}_beta{self.beta}.npy', Nxi_arr_save)
-            Nxi_arr = np.load(f'.Nxi_dz_Interp_init/NxiAtz{self.z:.2f}/Nxi_arr_Mv_{M_v:.3f}at_z={self.z:.2f}_A{self.A2byA1}_k{self.kMpc_trans}_alpha{self.alpha}_beta{self.beta}.npy')
-        Simpson_Interp = interp1d(self.deltaV_interp, Nxi_arr, kind='cubic')
-        return Simpson_Interp(delta_R)
-    
-    ### Minihalo Initialization
-    def Nxi_Pure(self,Mv,deltaR):
-        m_H = (m_p.to(u.M_sun)).value #M_sun
-        def Nxi_Pure_diff(m,Mv,deltaR):
-            return self.Xim(m,self.z)*m*self.dndmeps(m,Mv,deltaR,self.z)/ m_H * self.cosmo.omegab / self.cosmo.omegam
-        mslice = np.logspace(np.log10(self.M_J), np.log10(self.M_min), 12)
-        ans = np.zeros_like(deltaR)
-        for i in range(len(mslice)-1):
-            ans += quad_vec(Nxi_Pure_diff, mslice[i], mslice[i+1],args=(Mv,deltaR), epsrel=1e-6)[0]
-        return ans
-    
-    def Nxi_interp(self, Mv, deltaR):
-        try:
-            Nxi_arr = np.load(f'.Nxi_Interp_init/NxiAtz{self.z:.2f}/Nxi_arr_Mv_{Mv:.3f}at_z={self.z:.2f}_A{self.A2byA1}_k{self.kMpc_trans}_alpha{self.alpha}_beta{self.beta}.npy')
-        except FileNotFoundError:
-            os.makedirs(f'.Nxi_Interp_init/NxiAtz{self.z:.2f}', exist_ok=True)
-            nxi_pure = self.Nxi_Pure(Mv, self.deltaV_interp)
-            np.save(f'.Nxi_Interp_init/NxiAtz{self.z:.2f}/Nxi_arr_Mv_{Mv:.3f}at_z={self.z:.2f}_A{self.A2byA1}_k{self.kMpc_trans}_alpha{self.alpha}_beta{self.beta}.npy', nxi_pure)
-            Nxi_arr = np.load(f'.Nxi_Interp_init/NxiAtz{self.z:.2f}/Nxi_arr_Mv_{Mv:.3f}at_z={self.z:.2f}_A{self.A2byA1}_k{self.kMpc_trans}_alpha{self.alpha}_beta{self.beta}.npy')
-        Nxi_interp_Mv = interp1d(self.deltaV_interp, Nxi_arr, kind='cubic')
-        return Nxi_interp_Mv(deltaR) 
+    def nxi_dz_interp(self, deltaV, Mv):
+        def diff(m, Mv, deltaV):
+            return self.xim(m) * m * self.eps_dz(m, Mv, deltaV, self.z) / self.mH * self.ob0 / self.om0
+        return self._nxi_trapez(diff, Mv, deltaV)
+        
+    def nxi_interp(self, deltaV, Mv):
+        def diff(m, Mv, deltaV):
+            return self.xim(m, self.z) * m * self.cosmo.dndmeps(m, Mv, deltaV, self.z) / self.mH * self.ob0 / self.om0
+        return self._nxi_trapez(diff, Mv, deltaV)
     
     ### Minihaolo ST_over_PS
     def Nxi_ST(self,z):
@@ -121,7 +63,7 @@ class Ion:
         Min = self.cosmo.M_Jeans(z)
         Max = self.cosmo.M_vir(0.61,1e4,z)
         def Nxi_ST_diff(m):
-            return (self.Xim(m,z)*1 / m_H * self.cosmo.omegab / self.cosmo.omegam * m * self.cosmo.dndmst(m, z))
+            return (self.xim(m,z)*1 / m_H * self.cosmo.omegab / self.cosmo.omegam * m * self.cosmo.dndmst(m, z))
         mslice = np.logspace(np.log10(Min), np.log10(Max), 100)
         ans = 0
         for i in range(len(mslice)-1):
@@ -153,3 +95,61 @@ class Ion:
         dNxi_ST = self.dNxi_ST_Simpson(zlin)
         Nxi_st = abs(np.trapezoid(dNxi_ST, zlin)) + self.Nxi_ST(20)
         return Nxi_st
+
+
+    ### IGM term
+    def n_HI(self,deltaV):
+        return n_H(deltaV).to(u.Mpc**-3).value  ### coming number density of hydrogen atoms in cm^-3
+    
+    def CHII(self,z):
+        return 2.9*((1+z)/6)**-1.1
+
+    def dtdz(self,z):
+        return ((-1/(H(z)*(1+z))).to(u.s)).value
+
+    def dnrec_dz_path(self,deltaV,xHII_Field:np.ndarray)->np.ndarray:
+        x_HE = 1.08
+        CIGM = self.CHII(self.z)
+        # CIGM = 3.0
+        nh = self.n_HI(deltaV)*(u.Mpc**-3).to(u.cm**-3)
+        Q_HII = xHII_Field
+        alpha_A = 4.2e-13 #cm**3/s
+        # alpha_B = 2.59e-13  # cm^3/s at 10^4 K
+        differential_trans = self.dtdz(self.z)
+        return -CIGM*x_HE*alpha_A*nh*Q_HII*(1+self.z)**3 * differential_trans
+    
+
+    #### medium function
+    def xim(self,m):
+        A,B,C,D,E,F,G = 4.4 , 0.334, 0.023, 0.199, -0.042, 0,1
+        M7 = m/1e7
+        F0 = 1.0
+        return 1+A*M7**(B+C*np.log10(M7))*(F+G*(1+self.z)/10)* F0**(D+E*np.log10(F0))
+    
+    def fstar(self, Mh): # Donnan 25 
+        eps0 = 0.16
+        Mc = 10**11.7
+        beta = 0.9
+        gamma = 0.65
+        return 2*eps0 * ((Mh / Mc)**-beta + (Mh / Mc)**gamma)**-1
+
+
+    #### method functions
+    def _nxi_trapez(self, diff_func, Mv, deltaV):
+        x = np.linspace(np.log10(self.M_J), np.log10(self.M_min), 1000)
+        deltaV_grid, m_grid = np.meshgrid(self.deltaV_interp, x, indexing='ij')
+        m_vals = 10**m_grid
+        y = diff_func(m_vals, Mv, deltaV_grid) * m_vals * np.log(10)
+        integrand = np.trapezoid(y, x, axis=1)
+        interp_func = interp1d(self.deltaV_interp, integrand, kind='cubic', bounds_error=False, fill_value=0)
+        return interp_func(deltaV)
+    
+    def _nion_trapez(self, diff_func, Mv, deltaV):
+        Mh_max = 0.9*min(self.M_max,Mv)
+        x = np.linspace(np.log10(self.M_min), np.log10(self.M_max), 1000)
+        deltaV_grid, m_grid = np.meshgrid(self.deltaV_interp, x, indexing='ij')
+        m_vals = 10**m_grid
+        y = diff_func(m_vals, Mv, deltaV_grid) * m_vals * np.log(10)
+        integrand = np.trapezoid(y, x, axis=1)
+        interp_func = interp1d(self.deltaV_interp, integrand, kind='cubic', bounds_error=False, fill_value=0)
+        return interp_func(deltaV)
